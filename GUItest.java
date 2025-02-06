7bf163aa981c9800f59f83b70014cd50d4319223
Garryb057 <123600357+Garryb057@users.noreply.github.com>
2025-02-05 19:35:13 -0600

    Add files via upload

--- GUItest.java
+++ GUItest.java
@@ -6,19 +6,23 @@ import javax.swing.*;
 
 public class GUItest {
 	public static void main(String[] args) {
+		//Create and show GUI box
 		JFrame frame = new JFrame();
 		frame.setSize(400,400);
 		frame.setLayout(null);
 		frame.setVisible(true);
 		
+		//Create and add a text line
 		JLabel line1 = new JLabel("Test GUI");
 		line1.setBounds(10,5,100,10);
 		frame.add(line1);
 		
+		//Create and add a checkbox
 		Checkbox check1 = new Checkbox("Does this work?");
 		check1.setBounds(30,5,120,50);
 		frame.add(check1);
 		
+		//Create and add a set of radio buttons
 		JRadioButton rad1 = new JRadioButton();
 		JRadioButton rad2 = new JRadioButton();
 		JButton jButton = new JButton("Click");
@@ -34,9 +38,11 @@ public class GUItest {
 		frame.add(rad2);
 		frame.add(line2);
 		
+		//Create a button group with the radio buttons
 		rad.add(rad1);
 		rad.add(rad2);
 		
+		//Add a line to the GUI
 		JSeparator drawLine = new JSeparator();
 		drawLine.setBounds(0,140,400,50);
 		frame.add(drawLine);
